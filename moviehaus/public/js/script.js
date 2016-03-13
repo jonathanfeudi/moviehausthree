4e03cf5dec68c5af9fe8ece13d893e8a00a7a464
Ari Ingber <ariingber@gmail.com>
2016-03-12 21:27:22 -0500

    hitting api and sending data to a function in the app, having difficulty to get movies to render to page

--- moviehaus/public/js/script.js
+++ moviehaus/public/js/script.js
@@ -6,35 +6,57 @@ const App = React.createClass({
     return {movies:{}}
   },
 
-  searchMovie:function(name){
-    console.log(name)
-    $.get('/movies?t=' + name, function(data, status){
-        console.log(data)
-    });
-    // return !this.filterComplete(key)
+  renderMovieSearch:function(data) {
+    // console.log('render', data)
+    this.state.movies = JSON.parse(data);
+    this.setState({ movies: this.state.movies })
+    console.log(this.state.movies.Search[0].Title)
+    var b = this.state.movies.Search
+    // var array = [];
+    b.Search.forEach((movie)=> {
+      console.log(movie)
+    })
   },
+
 
   render:function() {
     return (
       <div>
+
         <h1>MovieHaus</h1>
-        <CreateMovieForm searchMovie={this.searchMovie}/>
+        <CreateMovieForm renderMovieSearch={this.renderMovieSearch}/>
+
+        <div>{}
+        </div>
+
       </div>
     )
   }
 });
 
 const CreateMovieForm = React.createClass({
-  movieSearch:function(event) {
+  handleSearch:function(event) {
     event.preventDefault();
     name = this.refs.name.value
-    this.props.searchMovie(name);
+    this.searchMovie(name);
     this.refs.movieForm.reset();
-
   },
+
+  searchMovie:function(name){
+    var that = this;
+    $.get('/movies?t=' + name, function(data, status ){
+        // console.log(data)
+    })
+    .done(function( data ) {
+      that.props.renderMovieSearch(data);
+    });
+
+    // return !this.filterComplete(key)
+  },
+
   render:function() {
     return (
-      <form className="movie-search" ref="movieForm" onSubmit={this.movieSearch}>
+      <form className="movie-search" ref="movieForm" onSubmit={this.handleSearch}>
         <div className="row">
           <div className="input-field col s4">
             <label htmlFor="movie_name">Search By Title</label>
